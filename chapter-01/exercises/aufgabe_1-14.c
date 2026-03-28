65eb6210d5bb8c22a59be07de0061cd8f3162a59
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-03-28 21:56:57 +0100

    finally solved exercsise 14

--- chapter-01/exercises/aufgabe_1-14.c
+++ chapter-01/exercises/aufgabe_1-14.c
@@ -1,8 +1,24 @@
-include<stdio.h>
-
+#include<stdio.h>
 int main(){
 
-return 0;
+	int c = 0;
+	int frequency[256] = {0};
 
+	while((c = getchar()) != EOF){
+		++frequency[c];
+		
+	
+	}
+	for (int i = 0; i < 256; ++i){
+		
+		if(frequency[i] > 0){
+			printf("Zeichen %c: ", i);
+			for (int j = 0; j < frequency[i]; j++){
+				printf("*");}
+		printf("\n");}
+			
+		
+	}
+	
 
 }
