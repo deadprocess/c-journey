a8b1f356f49a67d49d2b27a5e4db397ed6e58fa5
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-08-27 22:20:35 +0200

    solved exercise for 27th august 2026

---
+++ non-related-exercises/exercise-27-08-2026.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+int main (void) {
+	int all_tickets = 84;
+	int closed_tickets =57;
+	
+	
+	int open_tickets = all_tickets - closed_tickets;
+
+	printf("all tickets: %d\n", all_tickets);
+	printf("closed tickets: %d\n", closed_tickets);
+	printf("open tickets: %d\n", open_tickets);
+	if (open_tickets > closed_tickets) { printf("There are more open tickets than closed");  }
+	else { printf("There are more closed tickets than opened.");  }
+
+
+
+
+
+	return 0;
+
+
+}
