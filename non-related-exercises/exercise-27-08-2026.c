ee6d4abe84bb0369fdc0bf2506cac3e76453a188
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-08-28 23:29:05 +0200

    corrected small mistakes regarding exercise for 27th august 2026

--- non-related-exercises/exercise-27-08-2026.c
+++ non-related-exercises/exercise-27-08-2026.c
@@ -10,8 +10,8 @@ int main (void) {
 	printf("all tickets: %d\n", all_tickets);
 	printf("closed tickets: %d\n", closed_tickets);
 	printf("open tickets: %d\n", open_tickets);
-	if (open_tickets > closed_tickets) { printf("There are more open tickets than closed");  }
-	else { printf("There are more closed tickets than opened.");  }
+	if (open_tickets > closed_tickets) { printf("There are more open tickets than closed\n");  }
+	if (closed_tickets > open_tickets) { printf("There are more closed tickets than open.\n");  }
 
 
 
