2c4cff41552d9157727c63de5f34b03a0f5db44f
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-08-30 09:42:23 +0200

    added logic for bad input

--- non-related-exercises/exercise-28-08-2026.c
+++ non-related-exercises/exercise-28-08-2026.c
@@ -2,7 +2,14 @@
 
 int main (void) {
 
+    int gesamt;
 
+    printf("Gesamtzahl Tickets: ");
+    if (scanf("%d", &gesamt) != 1) {
+        printf("Falsches Eingabeformat!\n");
+        return 1;
+
+    }
 	return 0;
 
 
