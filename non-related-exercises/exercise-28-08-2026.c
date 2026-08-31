f0dea2e7d45977cbc86b4b037648905924a9d761
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-08-31 21:19:22 +0200

    solved exercise of 28th august 2026

--- non-related-exercises/exercise-28-08-2026.c
+++ non-related-exercises/exercise-28-08-2026.c
@@ -2,14 +2,45 @@
 
 int main (void) {
 
-    int gesamt;
+    int all_tickets = 0;
+    int closed_tickets = 0;
 
-    printf("Gesamtzahl Tickets: ");
-    if (scanf("%d", &gesamt) != 1) {
-        printf("Falsches Eingabeformat!\n");
+
+    printf("All tickets: ");
+    if (scanf("%d", &all_tickets) != 1) {
+        printf("Wrong input!\n");
         return 1;
 
+    }else
+    printf("Closed tickets: ");
+    if (scanf("%d", &closed_tickets) != 1) {
+        printf("Wrong input!\n");
+        return 1;
+
+
     }
+    int open_tickets = all_tickets - closed_tickets;
+
+    printf("all: %d\n", all_tickets);
+    printf("closed: %d\n", closed_tickets);
+    printf("open: %d\n", open_tickets);
+
+    if (closed_tickets > open_tickets) {
+        printf("There are more closed tickets than open.\n");
+
+
+    }
+    if (closed_tickets == open_tickets) {
+        printf("There are an equal amount of closed and open tickets.\n");
+
+
+    }
+    if (open_tickets > closed_tickets) {
+        printf("There are more open tickets than closed.\n");
+
+    }
+
+
 	return 0;
 
 
