fe325e0dba7f44a794c366957504e8897bbc1943
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-03 22:33:29 +0200

    added if statement

--- non-related-exercises/exercise-01-09-2026.c
+++ non-related-exercises/exercise-01-09-2026.c
@@ -4,6 +4,13 @@ int main (void) {
     int all_tickets;
     int closed_tickets;
     int open_tickets;
+    
+    if (scanf("%d", &all_tickets) != 1) {
+    
+
+    } 
+
+
     while (all_tickets != 0) {
 
 
