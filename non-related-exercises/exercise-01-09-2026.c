b24544c77caceeb89db0dee0463e79d237c0914a
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-04 21:21:23 +0200

    finished upper half of the exercise

--- non-related-exercises/exercise-01-09-2026.c
+++ non-related-exercises/exercise-01-09-2026.c
@@ -5,20 +5,23 @@ int main (void) {
     int closed_tickets;
     int open_tickets;
     
+    printf("All tickets: ");
     if (scanf("%d", &all_tickets) != 1) {
+        printf("Wrong input!");
+        return 1;
     
-
-    } 
-
-
+    }
+    printf("Closed tickets: ");
+    if (scanf("%d", &closed_tickets) != 1) {
+        printf("Wrong input!");
+        return 1;
+    }
+    
+    open_tickets = all_tickets - closed_tickets;
     while (all_tickets != 0) {
-
-
-
-
+    
+    
+    
     }
     return 0;
-
-
-
 }  
