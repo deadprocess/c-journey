aa9ffc95531d6202dc136e20e73126771dda50f4
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-05 22:31:33 +0200

    finished exercise 01-09-2026

--- non-related-exercises/exercise-01-09-2026.c
+++ non-related-exercises/exercise-01-09-2026.c
@@ -5,21 +5,37 @@ int main (void) {
     int closed_tickets;
     int open_tickets;
     
-    printf("All tickets: ");
+    printf("All tickets (0 to exit program): ");
     if (scanf("%d", &all_tickets) != 1) {
         printf("Wrong input!");
         return 1;
     
     }
-    printf("Closed tickets: ");
-    if (scanf("%d", &closed_tickets) != 1) {
-        printf("Wrong input!");
-        return 1;
-    }
     
-    open_tickets = all_tickets - closed_tickets;
+    
     while (all_tickets != 0) {
-    
+        printf("Closed_tickets: ");
+        if ( scanf("%d", &closed_tickets) != 1) {
+            printf("Wrong input!");  
+            return 1;   
+        }
+        open_tickets = all_tickets - closed_tickets;
+        
+        printf("All tickets: %d\n", all_tickets);
+        printf("Closed tickets: %d\n", closed_tickets);
+        printf("Open tickets: %d\n", open_tickets);
+        if (closed_tickets > open_tickets) {
+            printf("There are more closed tickets than open.\n");
+        }
+        if (open_tickets > closed_tickets) {
+            printf("There are more open tickets than closed.\n");
+
+        }
+        printf("All tickets: ");
+        if (scanf("%d", &all_tickets) != 1) {
+            printf("Wrong input!");
+            return 1;
+        }
     
     
     }
