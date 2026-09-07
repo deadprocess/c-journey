135c0d4887129bf930b40c553294e7b0d8459698
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-07 22:10:34 +0200

    prep & started exercise of 5th sep. 2026

---
+++ non-related-exercises/exercise-05-09-2026.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+int main(void) {
+
+    #define MAX_ENTRY 10
+    int all_tickets;
+    int closed_tickets;
+    int open_tickets;
+    int record[MAX_ENTRY];
+    int sum = 0;
+
+    printf("All_tickets: (0 to exit): ");
+    if (scanf("%d", &all_tickets) != 1) {
+        printf("wrong input!");
+        return 1;
+    }
+    
+    while(all_ticket != 0) {
+
+
+
+
+    }
+
+    
+
+
+
+
+
+
+
+
+
+
+
+}
