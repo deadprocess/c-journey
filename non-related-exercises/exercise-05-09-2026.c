0a55815388f5c713155da8bdeca0ea66b3b9e083
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-09 20:52:09 +0200

    progress on exercise 05-09-2026

--- non-related-exercises/exercise-05-09-2026.c
+++ non-related-exercises/exercise-05-09-2026.c
@@ -15,15 +15,29 @@ int main(void) {
         return 1;
     }
     
-    while(all_ticket != 0) {
+    while(all_tickets != 0) {
         
+        printf("Closed tickets: ");
         if (scanf("%d", &closed_tickets) != 1) {
             printf("wrong input!");
             return 1;
 
         }
-        open_tickets = all_tickets - closed_tickets
+        for (int i =0; i<= 9; i++) {
+            record[i] = all_tickets;
 
+
+        }
+        open_tickets = all_tickets - closed_tickets;
+        printf("all tickets: %d\n", all_tickets);
+        printf("closed tickets: %d\n", closed_tickets);
+        printf("open tickets: %d\n", open_tickets);
+        
+        printf("All_tickets: (0 to exit): ");
+        if (scanf("%d", &all_tickets) != 1) {
+            printf("wrong input!");
+            return 1;
+        } 
 
 
     }
