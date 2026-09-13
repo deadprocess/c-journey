e6f3dc84e08460337e8893c9367fa0349b4d405a
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-13 21:43:10 +0200

    corrected major mistakes such as false counting of array entries and false output of counted rounds etc.

--- non-related-exercises/exercise-05-09-2026.c
+++ non-related-exercises/exercise-05-09-2026.c
@@ -15,41 +15,41 @@ int main(void) {
         return 1;
     }
     
-    while(all_tickets != 0) {
-        for (int i = 0; i <= sum; i++){    
+    while(all_tickets != 0) { 
             
-            if (all_tickets == 0 && sum != 0) {
-                for (int x = 0; x < MAX_ENTRY; x++) {
-                    if (record[x] == 0) {
-                        return 0;
-                    }else {
-                        printf("%d.Round: %d\n", x + 1, record[x]);
-                    }
-                }
-                
-
-            }
-
-            printf("Closed tickets: ");
-            if (scanf("%d", &closed_tickets) != 1) {
-                printf("wrong input!");
-                return 1;
-            }
-            record[i] = all_tickets;
-	        sum = record[i];
-
         
-            open_tickets = all_tickets - closed_tickets;
-            printf("all tickets: %d\n", all_tickets);
-            printf("closed tickets: %d\n", closed_tickets);
-            printf("open tickets: %d\n", open_tickets);
-        
-            printf("All_tickets: (0 to exit): ");
-            if (scanf("%d", &all_tickets) != 1) {
-                printf("wrong input!");
-                return 1;
-            } 
 
+        printf("Closed tickets: ");
+        if (scanf("%d", &closed_tickets) != 1) {
+            printf("wrong input!");
+            return 1;
+        }
+        if (sum >= MAX_ENTRY) {
+            printf("Warnung!\n");
+        }else {
+            record[sum] = all_tickets;
+            sum++;
+        }
+        
+        
+        open_tickets = all_tickets - closed_tickets;
+        printf("all tickets: %d\n", all_tickets);
+        printf("closed tickets: %d\n", closed_tickets);
+        printf("open tickets: %d\n", open_tickets);
+        
+        printf("All_tickets: (0 to exit): ");
+        if (scanf("%d", &all_tickets) != 1) {
+            printf("wrong input!");
+            return 1;
         } 
+        
+         
     }
+    if (all_tickets == 0 && sum != 0) {
+            for (int x = 0; x <sum; x++) {
+                    printf("%d.Round: %d\n", x + 1, record[x]);
+                    
+            }
+    }
+    return 0;
 }
