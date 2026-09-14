3873b253f6cd3ff57482006559e80be1726c034c
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-14 17:52:09 +0200

    prep exercise 12-09-2026

---
+++ non-related-exercises/exercise-12-09-2026.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+
+int main(void) {
+
+#define MAX_ENTRIES 10
+int all_tickets;
+int closed_tickets;
+int open_tickets;
+int record[MAX_ENTRIES];
+int sum = 0;
+int i;
+    
+
+
+
+
+
+    return 0;
+
+}
