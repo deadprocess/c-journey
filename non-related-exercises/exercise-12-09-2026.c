23d618e35c3fe33a5a0c61f932aa4b0c44c84fe4
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-16 23:01:21 +0200

    fixed syntax error; implemented counting logic; added open tickets calculation

--- non-related-exercises/exercise-12-09-2026.c
+++ non-related-exercises/exercise-12-09-2026.c
@@ -10,15 +10,26 @@ int record[MAX_ENTRIES];
 int sum = 0;
 int i;
     
-    printf("All Tickets: (0 to exit)");
-    if (scanf("%d, &all_tickets") != 1) {
+    printf("All tickets: (0 to exit)");
+    if (scanf("%d", &all_tickets) != 1) {
         printf("Input was not a number.\n")
         return 1;
 
     }
     while(all_tickets != 0) {
+        printf("Closed tickets: "):
+        if (scanf("%d", &closed_tickets) != 1) {
+            printf("Input was not a number.\n");
+            return 1;
+        }
+        open_tickets = all_tickets - closed_ticket;
+        if (sum > MAX_ENTRIES) {
+            printf(" Warning: Entry won't be saved.")
 
-
+        }else {
+            record[sum] = open_tickets;
+            sum++;
+        }
 
     }
 
