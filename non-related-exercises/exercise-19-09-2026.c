cd210ca390d7f0be9f6dcfd3a4809508a925970e
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-21 19:16:37 +0200

    added prototypes (funcs); added first ticket query; added while loop

--- non-related-exercises/exercise-19-09-2026.c
+++ non-related-exercises/exercise-19-09-2026.c
@@ -1,11 +1,26 @@
 #include<stdio.h>
 #define MAX_ENTRIES 10
 
+int calculate_open_tickets(int all, int closed);
+void print_statistics(int all, int closed, int open);
+
 int main (void) {
 
+int all_tickets;
+int closed_tickets;
+int open_tickets;
+int records[MAX_ENTRIES];
+int sum = 0;
+
+    printf("All tickets (0 to exit); ");
+    if (scanf("%d", &all_tickets) != 1) {
+        printf("Wrong input.\n");
+        return 1;
+    }
+    while(all_tickets != 0) {
 
 
-
+    }
 
     return 0;
 
