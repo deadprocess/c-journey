fe281e0c78e1568ae7dd5be5156717985ca49679
deadprocess <56584652+deadprocess@users.noreply.github.com>
2026-09-25 21:57:55 +0200

    added define-line and prototypes

--- non-related-exercises/exercise-23-09-2026.c
+++ non-related-exercises/exercise-23-09-2026.c
@@ -1,5 +1,12 @@
 #include<stdio.h>
 
+#define MAX_ENTRIES 10
+
+int calculate_open(int all, int closed);
+void print_statistics(int all, int closed, int open);
+void print_records(int record[], int sum);
+
+
 int main() {
 
 
